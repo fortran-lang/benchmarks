648739c7b3e5cdd6139bc77e30c26ae888935a1c
Damian Rouson <damian@sourceryinstitute.org>
2021-06-28 20:36:31 -0700

    feat: make dy immutable via expression-association

--- poisson2d/modern.f90
+++ poisson2d/modern.f90
@@ -40,40 +40,44 @@ program poisson
   integer i,j
   integer, parameter :: M=150
   real(dp), parameter :: dx=0.01
-  real(dp) :: dy=dx
   real(dp) :: t_start, rho_sampled(M,M)
   
   call cpu_time(t_start)
 
-  do concurrent(i=1:M, j=1:M)
-    rho_sampled(i,j) = rho(i*dx,j*dy)
-  end do
- 
-  block ! Tighten the scoping to declutter the code above.
-    real(dp) :: delta_phi, t_end
-    real(dp), parameter :: epsilon0=8.85E-12_dp, tolerance=1E-6_dp
-    real(dp), dimension(M,M) :: phi_prime, phi
-    integer iteration
+  associate( dy => (dx) ) ! Associating with an expression provides immutable state so dy cannot be inadvertently redefined.
 
-    phi = 0.
-
-    phi_prime([1,M], 2:M-1) = phi([1,M], 2:M-1) ! Initialize only boundary values except corners. (Internal values will
-    phi_prime(2:M-1, [1,M]) = phi(2:M-1, [1,M]) ! be overwritten in the first iteration. Corners will never be used.)
-
-    delta_phi = tolerance + epsilon(tolerance) ! Ensure at least 1 iteration.
-    iteration = 0
-    do while (delta_phi > tolerance )
-      iteration = iteration + 1
-      do concurrent(i=2:M-1, j=2:M-1) ! Compute updated solution estimate at internal points.
-        phi_prime(i,j) = (phi(i+1,j) + phi(i-1,j) + phi(i,j+1) + phi(i,j-1))/4._dp + (dx/2._dp)*(dy/2._dp)/epsilon0*rho_sampled(i,j)
-      end do
-      delta_phi = maxval(abs(phi_prime - phi))
-      phi(2:M-1, 2:M-1) = phi_prime(2:M-1, 2:M-1) ! Update internal values.
+    do concurrent(i=1:M, j=1:M)
+      rho_sampled(i,j) = rho(i*dx,j*dy)
     end do
+ 
+    block ! Tighten the scoping to declutter the code above.
+      real(dp) :: delta_phi, t_end
+      real(dp), parameter :: epsilon0=8.85E-12_dp, tolerance=1E-6_dp
+      real(dp), dimension(M,M) :: phi_prime, phi
+      integer iteration
 
-    call cpu_time(t_end)
-    print *, t_end-t_start, iteration
+      phi = 0.
 
-  end block
+      phi_prime([1,M], 2:M-1) = phi([1,M], 2:M-1) ! Initialize only boundary values except corners. (Internal values will
+      phi_prime(2:M-1, [1,M]) = phi(2:M-1, [1,M]) ! be overwritten in the first iteration. Corners will never be used.)
+
+      delta_phi = tolerance + epsilon(tolerance) ! Ensure at least 1 iteration.
+      iteration = 0
+      do while (delta_phi > tolerance )
+        iteration = iteration + 1
+        do concurrent(i=2:M-1, j=2:M-1) ! Compute updated solution estimate at internal points.
+          phi_prime(i,j) =   (phi(i+1,j) + phi(i-1,j) + phi(i,j+1) + phi(i,j-1))/4._dp &
+                           + (dx/2._dp)*(dy/2._dp)/epsilon0*rho_sampled(i,j)
+        end do
+        delta_phi = maxval(abs(phi_prime - phi))
+        phi(2:M-1, 2:M-1) = phi_prime(2:M-1, 2:M-1) ! Update internal values.
+      end do
+
+      call cpu_time(t_end)
+      print *, t_end-t_start, iteration
+
+    end block
+
+  end associate
 
 end program
