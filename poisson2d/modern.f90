a7ccf54291d89fcf346f3f03be05d2393d12d2c7
Damian Rouson <damian@sourceryinstitute.org>
2021-06-28 19:55:55 -0700

    feat: add modernized poisson2d solver
    Refactorings
    ------------
    1. Use BLOCK for tighter variable scoping (reduces some clutter).
    2. Use array statements with vector subscripts to write compact
       initializations.
    3. Use DO CONCURRENT to express fine-grained parallelism and
       expose opportunities for optimizations such as vectorization,
       multithreading, or  GPU offloading.
    4. Replace some magic values, e.g., initial delta.
    5. Eliminate some unnecessary computation by initializing and
       updating only values that actually need initializing and
       updating.
    6. Use more compact conditional logic.
    7. Use a real kind specificaiton based on precision requirements
       rather than the ambiguously defined double-precision
       representation of any one given compiler on any one given
       platform.
    8. Eliminate unnecessary precision specifiers ("_dp") when an
       expression is simply a constant that is exactly representable
       in any reasonable floating-point representation.
    9. Separate the procedure interface into a module and the
       procedure definition into a submodule.**
    10. Use a more descriptive variable names in a few places.
    11. Add comments! (includeing some in FORD style)
    
    ** In larger projects, item 9 makes code much more user-friendly
    by isolation the application programmer interface (API) and
    reduces compile times by eliminating unnecessary compilation
    cascades.

---
+++ poisson2d/modern.f90
@@ -0,0 +1,79 @@
+module rho_interface
+  !! Define a scalar function over 2-space.
+  implicit none
+
+  private
+  public :: rho, dp
+
+  integer, parameter :: precision = 15, range = 307
+  integer, parameter :: dp = selected_real_kind(precision, range)
+
+  interface 
+    pure real(dp) module function rho(x,y)
+      !! Poisson equation inhomogeneous term.
+      implicit none
+      real(dp), intent(in) :: x,y
+    end function
+  end interface
+
+end module 
+
+submodule(rho_interface) rho_definition
+  implicit none
+contains
+  module procedure rho
+      !! To Do: give meaningful names to the magic numbers.
+      !! See https://en.wikipedia.org/wiki/Magic_number_(programming)#Unnamed_numerical_constants.
+      if (all([x,y]>0.6_dp .and. [x,y]<0.8_dp)) then
+        rho = 1.
+      else
+        rho = merge(-1., 0., all([x,y]>0.2_dp .and. [x,y]<0.4_dp))
+      end if
+  end procedure
+end submodule
+
+program poisson
+  !! Solve the 2D Poisson equation using a smoothing operation to iterate.
+  use rho_interface, only: rho, dp
+  implicit none
+
+  integer i,j
+  integer, parameter :: M=150
+  real(dp), parameter :: dx=0.01
+  real(dp) :: dy=dx
+  real(dp) :: t_start, rho_sampled(M,M)
+  
+  call cpu_time(t_start)
+
+  do concurrent(i=1:M, j=1:M)
+    rho_sampled(i,j) = rho(i*dx,j*dy)
+  end do
+ 
+  block ! Tighten the scoping to declutter the code above.
+    real(dp) :: delta_phi, t_end
+    real(dp), parameter :: epsilon0=8.85E-12_dp, tolerance=1E-6_dp
+    real(dp), dimension(M,M) :: phi_prime, phi
+    integer iteration
+
+    phi = 0.
+
+    phi_prime([1,M], 2:M-1) = phi([1,M], 2:M-1) ! Initialize only boundary values except corners. (Internal values will
+    phi_prime(2:M-1, [1,M]) = phi(2:M-1, [1,M]) ! be overwritten in the first iteration. Corners will never be used.)
+
+    delta_phi = tolerance + epsilon(tolerance) ! Ensure at least 1 iteration.
+    iteration = 0
+    do while (delta_phi > tolerance )
+      iteration = iteration + 1
+      do concurrent(i=2:M-1, j=2:M-1) ! Compute updated solution estimate at internal points.
+        phi_prime(i,j) = (phi(i+1,j) + phi(i-1,j) + phi(i,j+1) + phi(i,j-1))/4._dp + (dx/2._dp)*(dy/2._dp)/epsilon0*rho_sampled(i,j)
+      end do
+      delta_phi = maxval(abs(phi_prime - phi))
+      phi(2:M-1, 2:M-1) = phi_prime(2:M-1, 2:M-1) ! Update internal values.
+    end do
+
+    call cpu_time(t_end)
+    print *, t_end-t_start, iteration
+
+  end block
+
+end program
